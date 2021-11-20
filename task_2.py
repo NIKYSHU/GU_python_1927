222c6e5c7e860f88abd05990b18be5b9bf1c8536
NIKYSHU <nikyshu@gmail.com>
2021-11-20 17:01:41 +0300

    task_1.3

--- task_2.py
+++ task_2.py
@@ -13,18 +13,13 @@ for element in odd_numbers:
     if amount_digits % 7 == 0:
         amount_digits_7 += odd_numbers[i]
     i += 1
-
 print('Cписок, из кубов нечётных чисел: ', odd_numbers)
-
 print('Сумма чисел: ', amount_digits_7)
-
 i = 0
 for element in odd_numbers:
     odd_numbers[i] = element + 17
     i += 1
-
 print('Cписок, из кубов нечётных чисел +17: ', odd_numbers)
-
 i = 0
 amount_digits_17 = 0
 for element in odd_numbers:
@@ -36,5 +31,4 @@ for element in odd_numbers:
     if amount_digits % 7 == 0:
         amount_digits_17 += odd_numbers[i]
     i += 1
-
 print('Сумма чисел: ', amount_digits_17)
